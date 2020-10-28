a0543964facd3f9af5898be1ea802fa122c6d20f
Turlybek Zhuranov <turlybekk@gmail.com>
2020-10-29 01:32:07 +0600

    fix: update count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  return [].concat(...matrix).filter(cat => cat === '^^').length;
 };
